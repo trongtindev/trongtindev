6412efe5e71e9b3bea68df13f528a2b5cc656096
Tin Nguyen Trong <me@trongtin.dev>
2024-11-15 20:02:57 +0700

    feat: add auth

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,12 +2,13 @@ import jsonwebtoken from 'jsonwebtoken';
 import { IAuth } from '../interfaces/auth';
 
 export default defineEventHandler((event) => {
-  const { AUTH_TOKEN } = useRuntimeConfig();
+  if (!event.path.startsWith('/api')) return;
 
   const authorization = getRequestHeader(event, 'authorization');
   if (!authorization) throw createError({ status: 401 });
 
   try {
+    const { AUTH_TOKEN } = useRuntimeConfig();
     event.context.auth = jsonwebtoken.verify(
       authorization.split(' ')[1],
       AUTH_TOKEN
