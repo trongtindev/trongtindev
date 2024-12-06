902e4ecd152e0ff9170657b15b74cfbabbb1a6e1
Tin Nguyen Trong <me@trongtin.dev>
2024-12-06 18:33:35 +0700

    initial commit

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,12 +1,16 @@
 import { MongoClient } from 'mongodb';
 import assert from 'assert';
-export const getDb = async () => {
-  const { MONGODB_URI, MONGODB_NAME } = useRuntimeConfig();
 
+let client: MongoClient;
+
+export const getDb = async () => {
+  if (client) return;
+
+  const { MONGODB_URI, MONGODB_NAME } = useRuntimeConfig();
   assert(MONGODB_URI);
   assert(MONGODB_NAME);
 
-  const client = new MongoClient(MONGODB_URI);
+  client = new MongoClient(MONGODB_URI, { maxIdleTimeMS: 60 * 5 * 1000 });
   await client.connect();
   return client.db(MONGODB_NAME);
 };
